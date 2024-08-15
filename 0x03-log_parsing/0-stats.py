85eb833fa8fa76f45a176e02c7a795f433d1b2f0
El-sharqawy <elsharqawy2@gmail.com>
2024-08-16 00:31:57 +0300

    Stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,55 +1,13 @@
 #!/usr/bin/python3
-"""A Script that Parses Log File"""
+"""parse a log file"""
 
-import re
+import sys
 
+if __name__ == "__main__":
+    file_Size = 0
+    count = 0
 
-def extInput(line):
-    """Extracts line of HTTP Request"""
-    fp = (
-        r"\s*(?P<ip>\S+)\s*",
-        r"\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]",
-        r'\s*"(?P<request>[^"]*)"\s*',
-        r"\s*(?P<status_code>\S+)",
-        r"\s*(?P<file_size>\d+)",
-    )
-    info = {
-        "status_code": 0,
-        "file_size": 0,
-    }
-    log_fmt = "{}\\-{}{}{}{}\\s*".format(fp[0], fp[1], fp[2], fp[3], fp[4])
-    result_match = re.fullmatch(log_fmt, line)
-    if result_match is not None:
-        status_code = result_match.group("status_code")
-        file_size = int(result_match.group("file_size"))
-        info["status_code"] = int(status_code)
-        info["file_size"] = file_size
-    return info
-
-
-def print_stats(file_size, stats_codes):
-    """Print HTTP Request Log Statistics"""
-    print("File size: {:d}".format(file_size), flush=True)
-    for code in sorted(stats_codes):
-        num = stats_codes.get(code, 0)
-        if num > 0:
-            print("{:s}: {:d}".format(code, num), flush=True)
-
-
-def update(line, file_size, stats_codes):
-    """Updates the metrics from HTTP Requst Log"""
-    lineInfo = extInput(line)
-    code = lineInfo.get("status_code", 0)
-    if code in stats_codes.keys():
-        stats_codes[code] += 1
-    return file_size + lineInfo["file_size"]
-
-
-def main():
-    """Main Def"""
-    line_num = 0
-    file_size = 0
-    stats_codes = {
+    stats = {
         "200": 0,
         "301": 0,
         "400": 0,
@@ -60,16 +18,29 @@ def main():
         "500": 0,
     }
 
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(file_Size))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
+
     try:
-        while True:
-            line = input()
-            file_size = update(line, file_size, stats_codes)
-            line_num += 1
-            if line_num % 10 == 0:
-                print_stats(file_size, stats_codes)
-    except (KeyboardInterrupt, EOFError):
-        print_stats(file_size, stats_codes)
-
-
-if __name__ == "__main__":
-    main()
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                file_Size += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, file_Size)
+        print_stats(stats, file_Size)
+    except KeyboardInterrupt:
+        print_stats(stats, file_Size)
+        raise
