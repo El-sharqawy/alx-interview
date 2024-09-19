6b980dd0b13c463b40740e9276bb7bb6dfccc093
El-sharqawy <elsharqawy2@gmail.com>
2024-09-20 00:01:47 +0300

    Making Change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""consider it as useful documentation please"""
+
+
+def makeChange(coins, total):
+    """consider it as a useful documentation idk"""
+    if total <= 0:
+        return 0
+
+    chk, mvoes = 0, 0
+    coins.sort(reverse=True)
+
+    for coin in coins:
+        while chk < total:
+            chk += coin
+            mvoes += 1
+
+        if chk == total:
+            return mvoes
+
+        chk -= coin
+        mvoes -= 1
+
+    return -1
