66fa638651af7d6e1e0811127cf2732c8d336708
El-sharqawy <elsharqawy2@gmail.com>
2024-09-27 01:12:33 +0300

    Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+a function def island_perimeter(grid): that returns the perimeter of the island described in grid
+"""
+
+
+def island_perimeter(grid):
+    """get the perimeter"""
+    count = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                count += 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    count -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    count -= 2
+    return count
